11798b284a1bc295a33f2bc330018546bdb891c1
kishlay <kishlay141@gmail.com>
2025-03-28 17:41:10 +0530

    removed the node modules

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Connect to MongoDB
 mongoose
